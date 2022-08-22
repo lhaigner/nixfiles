b8b60de150943792c924e797fe25f3e42f19f89c
Lukas Haigner <code@firy.io>
2022-08-22 20:54:38 +0200

    Fix efi mount point

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -13,8 +13,14 @@
     ];
 
   # Bootloader.
-  boot.loader.systemd-boot.enable = true;
-  boot.loader.efi.canTouchEfiVariables = true;
+  boot.loader = {
+    systemd-boot.enable = true;
+
+    efi = {
+      canTouchEfiVariables = true;
+      efiSysMountPoint = "/boot/efi";
+    };
+  };
 
   # Enable networking.
   networking.networkmanager.enable = true;
